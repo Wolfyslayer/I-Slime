b6c124740f7792e3fd2d0985f22f7d88deabaa37
Wolfyslayer <simon.eskils00@gmail.com>
2025-07-31 22:31:37 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // projekrotsmapp
+  build: {
+    outDir: 'build',  // ändra output mappen från dist till build
+  },
 })
