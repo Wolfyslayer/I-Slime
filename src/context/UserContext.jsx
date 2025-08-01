3b92344fda210ea7bc6096d9aad64c8945299d2b
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 21:12:50 +0200

    Update UserContext.jsx

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,4 +1,16 @@
 // src/context/UserContext.jsx
-import { createContext } from 'react'
+import { createContext, useContext, useState } from 'react'
 
 export const UserContext = createContext(null)
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const useUser = () => useContext(UserContext)
