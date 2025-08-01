414699ad2df6ce42e0169b98501f2e63c9046229
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 18:46:08 +0200

    Create data.js

---
+++ src/data/data.js
@@ -0,0 +1,47 @@
+export const classData = {
+  Archer: {
+    Hunter: ["Stalker", "Grand Archer", "Demon Hunter"],
+    "Gun Wielder": ["Magic Gunslinger", "Gunner", "Human Weapon"]
+  },
+  Swordsman: {
+    Knight: ["Knight Commander", "Paladin", "Knight of Light"],
+    Warrior: ["Martial Artist", "Berserker", "War God"]
+  },
+  Mage: {
+    Sage: ["Alchemist", "High Priest", "Angel of Devotion"],
+    Wizard: ["Elementalist", "Sorcerer", "Magic Destroyer"]
+  }
+};
+
+export const classImages = {
+  Archer: "https://i.imgur.com/Usk0waB.png",
+  Swordsman: "https://i.imgur.com/T6vT1CA.png",
+  Mage: "https://i.imgur.com/xyEz0J6.png"
+};
+
+export const pathImages = {
+  Hunter: "https://i.imgur.com/qBjyigF.png",
+  "Gun Wielder": "https://i.imgur.com/MPcV6n5.png",
+  Knight: "https://i.imgur.com/BKZAjlN.png",
+  Warrior: "https://i.imgur.com/YMHcEB3.png",
+  Sage: "https://i.imgur.com/3EXOk7N.png",
+  Wizard: "https://i.imgur.com/owmP2ou.png"
+};
+
+export const petsData = [
+  { name: "Pet1", img: "https://i.imgur.com/qBjyigF.png" },
+  { name: "Pet2", img: "https://i.imgur.com/MPcV6n5.png" },
+  { name: "Pet3", img: "https://i.imgur.com/BKZAjlN.png" },
+  { name: "Pet4", img: "https://i.imgur.com/YMHcEB3.png" },
+  { name: "Pet5", img: "https://i.imgur.com/3EXOk7N.png" },
+  { name: "Pet6", img: "https://i.imgur.com/owmP2ou.png" }
+];
+
+export const skillsData = [
+  { name: "Skill1", img: "https://i.imgur.com/4mgxq9G.png" },
+  { name: "Skill2", img: "https://i.imgur.com/7fRUWjJ.png" },
+  { name: "Skill3", img: "https://i.imgur.com/FC7zZ3R.png" },
+  { name: "Skill4", img: "https://i.imgur.com/0jjC9Yj.png" },
+  { name: "Skill5", img: "https://i.imgur.com/2vLy0bk.png" },
+  { name: "Skill6", img: "https://i.imgur.com/jXIpAtY.png" }
+];
