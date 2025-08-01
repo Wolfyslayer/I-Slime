1aeb67ccb6920afc44f2d6bc23839c4c77da6222
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 11:51:13 +0200

    Update MyBuilds.jsx

--- src/components/MyBuilds.jsx
+++ src/components/MyBuilds.jsx
@@ -1,1 +1,13 @@
+import React from 'react'
+
+export default function MyBuilds() {
+  return (
+    <div className="main-content">
+      <h1>Mina Builds</h1>
+      <div className="card">
+        <p>Här visas dina sparade builds.</p>
+      </div>
+    </div>
+  )
+}
 
