aac429d89e687b33429f633b48def4f9fba16c03
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 15:17:13 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-// src/index.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // Om du har global CSS
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import './lib/i18n'  // Importera i18n här
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
