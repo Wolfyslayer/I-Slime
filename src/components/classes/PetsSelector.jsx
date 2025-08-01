f36b9657715618e6ee74d6d18b73042015ea654d
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 17:38:12 +0200

    Update PetsSelector.jsx

--- src/components/classes/PetsSelector.jsx
+++ src/components/classes/PetsSelector.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+
+const pets = [
+  { id: 'wolf', name: 'Wolf', img: '/images/pet_wolf.png' },
+  { id: 'dragon', name: 'Dragon', img: '/images/pet_dragon.png' },
+  { id: 'fox', name: 'Fox', img: '/images/pet_fox.png' },
+];
+
+export default function PetsSelector({ selectedPet, onSelectPet }) {
+  return (
+    <div className="section">
+      <h2>Select Your Pet</h2>
+      <div className="choices">
+        {pets.map((pet) => (
+          <div
+            key={pet.id}
+            className={`choice ${selectedPet === pet.id ? 'selected' : ''}`}
+            onClick={() => onSelectPet(pet.id)}
+          >
+            <img src={pet.img} alt={pet.name} />
+            <span>{pet.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
