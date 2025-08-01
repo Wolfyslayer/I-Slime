7f48cc0bab5b4746aa1dbd5e6a6bd9650f1accfc
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 13:30:13 +0200

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -4,3 +4,9 @@ const supabaseUrl = 'https://biyboqoelvcwftcmwhbx.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpeWJvcW9lbHZjd2Z0Y213aGJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM5NTUyMjQsImV4cCI6MjA2OTUzMTIyNH0.XjP5iJY9vivhkejv5VFqRpYeaoDBgTQUx620vFASHLc'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+// Lägg in admin user IDs här:
+export const ADMIN_USERS = [
+  'admin-user-uuid-1',
+  'admin-user-uuid-2',
+]
