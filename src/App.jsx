c36d3e197816a6d18bcf07d2f1f15f4fbb24af8d
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 12:04:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 import ProtectedRoute from './routes/ProtectedRoute'
 import Register from './components/Register'
 import Login from './components/Login'
 import CreateBuild from './components/CreateBuild'
 import BuildList from './components/BuildList'
-import './index.css'
 import MyBuilds from './components/MyBuilds'
-
-<Route
-  path="/my-builds"
-  element={
-    <ProtectedRoute>
-      <MyBuilds />
-    </ProtectedRoute>
-  }
-/>
-
+import './index.css'
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<BuildList />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/create-build"
-          element={
-            <ProtectedRoute>
-              <CreateBuild />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <Sidebar />
+      <main style={{ marginLeft: '220px', padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<BuildList />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/create-build"
+            element={
+              <ProtectedRoute>
+                <CreateBuild />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/my-builds"
+            element={
+              <ProtectedRoute>
+                <MyBuilds />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </main>
     </Router>
   )
 }
