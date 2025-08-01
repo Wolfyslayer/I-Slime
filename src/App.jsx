58227ca127b5c211c1ac4917e5a295ae9dbbfb9a
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 10:32:06 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
-// App.jsx
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import CreateBuild from './components/CreateBuild'
+import Home from './components/Home'
+import Login from './components/Login'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create" element={<CreateBuild />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
