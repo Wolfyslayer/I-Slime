7ca5226c999e864ecc5bba47df5d3d09519541e2
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 16:30:20 +0200

    Update ProtectedRoute.jsx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,41 +1,11 @@
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { supabase } from '../lib/supabaseClient'
+import supabase from '../lib/supabaseClient'
 
 export default function ProtectedRoute({ children }) {
-  const [loading, setLoading] = useState(true)
-  const [allowed, setAllowed] = useState(false)
   const user = supabase.auth.user()
-
-  useEffect(() => {
-    async function checkBan() {
-      if (!user) {
-        setAllowed(false)
-        setLoading(false)
-        return
-      }
-      const { data } = await supabase
-        .from('banned_users')
-        .select('*')
-        .eq('user_id', user.id)
-        .single()
-
-      setAllowed(!data) // Om finns i banned_users → false
-      setLoading(false)
-    }
-    checkBan()
-  }, [user])
-
-  if (loading) return <p>Laddar...</p>
-
   if (!user) {
-    return <Navigate to="/login" />
+    return <Navigate to="/login" replace />
   }
-
-  if (!allowed) {
-    return <p>Du är bannlyst och har inte tillgång.</p>
-  }
-
   return children
 }
