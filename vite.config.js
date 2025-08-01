4e128f7a6036e8567d02fb57cea789a959016c88
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 11:07:43 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  base: './', // ← Lägg till detta för att routing ska fungera på Vercel!
 })
