a3cc0fb3b4093d11397a67ed8ef6400af777d8cf
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-01 17:40:06 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,14 @@ import Login from './components/Login'
 import CreateBuild from './components/CreateBuild'
 import BuildList from './components/BuildList'
 import MyBuilds from './components/MyBuilds'
+import ClassSelector from './components/classes/ClassSelector';
+import PathSelector from './components/classes/PathSelector';
+import PetsSelector from './components/classes/PetsSelector';
+import SkillsSelector from './components/classes/SkillsSelector';
+import ItemsEditor from './components/classes/ItemsEditor';
+import './styles/theme.css';
 import AdminPanel from './components/AdminPanel'
 import { useTranslation } from 'react-i18next'
-import './index.css'
 
 export default function App() {
   const [sidebarOpen, setSidebarOpen] = useState(false)
