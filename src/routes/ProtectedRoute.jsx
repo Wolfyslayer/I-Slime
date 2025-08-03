c65598322128e3e851221a1c4d141833658375f2
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-03 14:07:52 +0200

    Update ProtectedRoute.jsx

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,12 +1,16 @@
+// src/routes/ProtectedRoute.jsx
 import React from 'react'
 import { Navigate } from 'react-router-dom'
-import { supabase } from '../lib/supabaseClient'
-
+import { useUser } from '../context/UserContext'
 
 export default function ProtectedRoute({ children }) {
-  const user = supabase.auth.user()
+  const { user, loading } = useUser()
+
+  if (loading) return null // eller en spinner
+
   if (!user) {
     return <Navigate to="/login" replace />
   }
+
   return children
 }
