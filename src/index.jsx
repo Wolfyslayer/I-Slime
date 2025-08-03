319f4793be538d39a97a0ecdb36a88b72c085ad6
Wolfyslayer <simon.eskils00@gmail.com>
2025-08-03 14:38:26 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { UserProvider } from './context/UserContext'
-import './lib/i18n' // om du använder i18n för översättning
 import './styles/theme.css'
 
-const root = document.getElementById('root')
-
-if (root) {
-  const reactRoot = ReactDOM.createRoot(root)
-  reactRoot.render(
-    <React.StrictMode>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </React.StrictMode>
-  )
-}
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
+)
